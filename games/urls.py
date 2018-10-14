ce2f5fb85ee34186b031872d9b3da43ab1769cb0
Daniel Bong <bongster88@gmail.com>
2018-10-14 21:22:05 +0900

    add layou

--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import GameView
+from .views import GameView, WODView, LeaderboardView, GameRedirectView
 
 urlpatterns = [
-    path('', GameView.as_view()),
+    path('<int:pk>/leaderboard', LeaderboardView.as_view(), name='game-leaderboard'),
+    path('<int:pk>/wod', WODView.as_view(), name='game-wod'),
+    path('<int:pk>', GameView.as_view(), name='game-detail'),
+    path('', GameRedirectView.as_view(), name='games'),
 ]
 
--- games/views.py
+++ games/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 # Create your views here.
 
 
+class GameRedirectView(RedirectView):
+    permanent = False
+    query_string = True
+    pattern_name = 'game-detail'
+
+    def get_redirect_url(self, *args, **kwargs):
+        kwargs['pk'] = kwargs.get('pk') or 1
+        print(kwargs)
+        return super().get_redirect_url(*args, **kwargs)
+
 class GameView(TemplateView):
-    template_name = 'game.html'
+    template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super(GameView, self).get_context_data(**kwargs)
+        return context
+
+class LeaderboardView(TemplateView):
+    template_name = 'leaderboard/index.html'
+
+class WODView(TemplateView):
+    template_name = 'wod/index.html'
