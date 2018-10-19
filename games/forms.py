d505d19a16f26af9aa65576b7b33d522a351e2a2
bongster88 <bongster88@gmail.com>
2018-10-20 01:24:54 +0900

    wip: wod ui

--- games/forms.py
+++ games/forms.py
@@ -4,29 +4,30 @@ from django import forms
 from .models import Game, WOD, WOD2Game, Team, Team2Game, Record
 
 
-class CustomModelChoiceField(forms.ModelChoiceField):
+class CustomChoiceField(forms.ChoiceField):
     def label_from_instance(self, obj):
          return "%s %s" % (obj.id, obj.name)
 
+
 class WOD2GameForm(forms.ModelForm):
-    wod_id = CustomModelChoiceField(queryset=WOD.objects.all())
-    game_id = CustomModelChoiceField(queryset=Game.objects.all())
+    wod_id = CustomChoiceField(choices=( (obj.id, obj.name ) for obj in WOD.objects.all()))
+    game_id = CustomChoiceField(choices=( (obj.id, obj.name ) for obj in Game.objects.all()))
 
     class Meta:
         model = WOD2Game
         fields = '__all__'
 
 class Team2GameForm(forms.ModelForm):
-    team_id = CustomModelChoiceField(queryset=Team.objects.all())
-    game_id = CustomModelChoiceField(queryset=Game.objects.all())
+    team_id = CustomChoiceField(choices=( (obj.id, obj.name ) for obj in Team.objects.all()))
+    game_id = CustomChoiceField(choices=( (obj.id, obj.name ) for obj in Game.objects.all()))
 
     class Meta:
         model = Team2Game
         fields = '__all__'
 
 class RecordForm(forms.ModelForm):
-    team_id = CustomModelChoiceField(queryset=Team.objects.all())
-    wod_id = CustomModelChoiceField(queryset=WOD.objects.all())
+    team_id = CustomChoiceField(choices=( (obj.id, obj.name ) for obj in Team.objects.all()))
+    wod_id = CustomChoiceField(choices=( (obj.id, obj.name ) for obj in WOD.objects.all()))
 
     class Meta:
         model = Record
--- games/views.py
+++ games/views.py
@@ -1,7 +1,13 @@
+import logging
+
 from django.shortcuts import render
 from django.views.generic import TemplateView, RedirectView
 
+from .models import Game, WOD2Game, WOD
+
 # Create your views here.
+
+logger = logging.getLogger(__name__)
 
 
 class GameRedirectView(RedirectView):
@@ -13,14 +19,50 @@ class GameRedirectView(RedirectView):
         kwargs['pk'] = kwargs.get('pk') or 1
         return super().get_redirect_url(*args, **kwargs)
 
-class GameView(TemplateView):
+class GameDetailView(TemplateView):
     template_name = 'index.html'
     def get_context_data(self, **kwargs):
-        context = super(GameView, self).get_context_data(**kwargs)
+        context = super(GameDetailView, self).get_context_data(**kwargs)
+        context['game'] = Game.objects.get(pk=kwargs['pk'])
+        print(context['game'])
         return context
 
 class LeaderboardView(TemplateView):
     template_name = 'leaderboard/index.html'
+    def get_context_data(self, **kwargs):
+        context = super(LeaderboardView, self).get_context_data(**kwargs)
+        print(context)
+        context['game'] = Game.objects.get(pk=kwargs['game_id'])
+        context['leaderboard'] = []
+        return context
 
 class WODView(TemplateView):
     template_name = 'wod/index.html'
+    def get_context_data(self, **kwargs):
+        context = super(WODView, self).get_context_data(**kwargs)
+        game = Game.objects.get(pk=kwargs['game_id'])
+        wod_ids = WOD2Game.objects.filter(
+            game_id=game.id,
+            is_ative=True,
+        ).order_by('order').values_list('wod_id', flat=True)
+        print(wod_ids)
+
+        context['game'] = game
+        wods = WOD.objects.filter(
+            pk__in=wod_ids,
+        )
+        context_wods = {}
+        for wod_id in wod_ids:
+            wod = list(filter(lambda x: x.id == wod_id, wods))[0]
+            context_wods[wod.id] = wod
+
+        context['wods'] = context_wods
+
+        return context
+
+class WODDetailView(TemplateView):
+    template_name = 'wod/index.html'
+    def get_context_data(self, **kwargs):
+        context = super(WODDetailView, self).get_context_data(**kwargs)
+        context['game'] = Game.objects.get(pk=kwargs['game_id'])
+        return context
