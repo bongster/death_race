437c912888ec6696208d0e26ca474897420379e9
bongster88 <bongster88@gmail.com>
2019-01-27 15:35:23 +0700

    remove unused code

--- death_race/urls.py
+++ death_race/urls.py
@@ -19,13 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import MainView, MainRedirectView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
